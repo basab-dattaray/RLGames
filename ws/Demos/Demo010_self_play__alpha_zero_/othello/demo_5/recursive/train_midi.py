fe4bc9c49e7625d3a122a692c4ad0268f83ba4a7
basab-dattaray <basab.dattaray@gmail.com>
2020-07-07 08:30:16 -0700

    REL .4: added self_play with Othello - it works with minimum fixed args, both for pit and main

---
+++ ws/Demos/Demo010_self_play__alpha_zero_/othello/demo_5/recursive/train_midi.py
@@ -0,0 +1,24 @@
+from ws.Demos.Demo010_self_play__alpha_zero_.othello.demo_5.recursive.ConfigParams import ConfigParams
+from ws.RLAgents.self_play.alpha_zero.Agent import Agent
+
+if __name__ == "__main__":
+    Agent.fn_init(__file__, ConfigParams()) \
+ \
+        .fn_clean() \
+        .fn_create_new_session() \
+        .fn_change_configs(configs=
+    {
+        'model_acceptance_win_ratio': 0.55,
+        'num_training_eval_games': 10,
+        'POLICY_RULES': {  # PolicyAlgos: MCTS, DIRECT_MODEL, MCTS_RECURSIVE
+            'POLICY_TRAINING_EXECUTION': 'MCTS_RECURSIVE',
+            'POLICY_TRAINING_EVAL': 'MCTS_RECURSIVE'},
+
+        'num_episodes': 6,
+        'num_iterations': 3,
+        'epochs': 5,
+        'num_of_mcts_simulations': 25,
+    }) \
+        .fn_train() \
+ \
+        .fn_archive_it()
