f268f7a0c8cae089530818cc217ec8a2c12afa17
basab-dattaray <basab.dattaray@gmail.com>
2020-07-07 12:53:16 -0700

    WIP .4.9:

--- ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/pit.py
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/pit.py
@@ -17,34 +17,35 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-mini_othello = False  # Play in 6x6 instead of the normal 8x8.
-human_vs_cpu = True
-
+# mini_othello = False  # Play in 6x6 instead of the normal 8x8.
+# human_vs_cpu = True
+# all players
+fn_random_player_policy = lambda g: RandomPlayer(g).play
+fn_greedy_player_policy = lambda g: GreedyPlayer(g).play
+fn_human_player_policy = lambda g: HumanPlayer(g).play
 dir_path, app_name = AppInfo.fn_get_path_and_app_name(__file__)
 
 game = OthelloGame(args.board_size)
 
 
-# all players
-random_player_policy = lambda g: RandomPlayer(g).play
-greedy_player_policy = lambda g: GreedyPlayer(g).play
-human_player_policy = lambda g: HumanPlayer(g).play
+
 
 
 
 # nnet players
-n1 = NeuralNetWrapper(args, game)
+system_nn = NeuralNetWrapper(args, game)
 
 
 
-n1.load_checkpoint('tmp/','best.pth.tar')
+system_nn.load_checkpoint('tmp/', 'best.pth.tar')
 
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
-mcts1 = MCTS(game, n1, args1)
-n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+# args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+mcts1 = MCTS(game, system_nn, args)
+fn_get_action_probabilities_for_system = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
+human_vs_cpu = True
 if human_vs_cpu:
-    player2 = human_player_policy(game)
+    player2 = fn_human_player_policy(game)
 else:
     n2 = NeuralNetWrapper(game)
     n2.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
@@ -54,6 +55,6 @@ else:
 
     player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
 
-arena = Arena.Arena(n1p, player2, game, display=OthelloGame.display)
+arena = Arena.Arena(fn_get_action_probabilities_for_system, player2, game, display=OthelloGame.display)
 
 print(arena.playGames(2, verbose=True))
