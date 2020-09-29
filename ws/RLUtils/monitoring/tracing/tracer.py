ebeb38432e57cb4bea970d07a4ad9e913a104e31
basab-dattaray <basab.dattaray@gmail.com>
2020-09-29 12:10:23 -0700

    Mac 1.6.8.1:

--- ws/RLUtils/monitoring/tracing/tracer.py
+++ ws/RLUtils/monitoring/tracing/tracer.py
@@ -4,11 +4,12 @@ import inspect
 
 def tracer(args):
     # recorder = args['fn_recorder']
+
     recorder = args.recorder
     def function_wrapper_maker(fn):
         @functools.wraps(fn)
         def fn_wrapper(*args, **kwargs):
-            recorder.fn_record_func_title_begin('abc')
+            recorder.fn_record_func_title_begin(fn.__name__)
 
             ret_value = fn(*args, **kwargs)
             recorder.fn_record_func_title_end()
