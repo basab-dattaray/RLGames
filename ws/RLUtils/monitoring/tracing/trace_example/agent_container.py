c7461b3bb04d119556a7cfef126fbc2e720f9993
basab-dattaray <basab.dattaray@gmail.com>
2020-09-28 22:05:09 -0700

    Mac 1.6.5.7:

--- ws/RLUtils/monitoring/tracing/trace_example/agent_container.py
+++ ws/RLUtils/monitoring/tracing/trace_example/agent_container.py
@@ -1,17 +1,17 @@
 from collections import namedtuple
 
-from ws.RLUtils.monitoring.tracing.wrapper_maker import trace
+from ws.RLUtils.monitoring.tracing.tracer import tracer
 
 
 def agent_container(fn_recorder):
     agent_container_ref = namedtuple('_', ['fn_test1','fn_test2'])
 
-    @trace(fn_recorder)
+    @tracer(fn_recorder)
     def fn_test1():
         print('RUNNING fn_test1')
         return agent_container_ref
 
-    @trace(fn_recorder)
+    @tracer(fn_recorder)
     def fn_test2():
         print('RUNNING fn_test2')
         return agent_container_ref
--- ws/RLUtils/monitoring/tracing/tracer.py
+++ ws/RLUtils/monitoring/tracing/tracer.py
@@ -1,12 +1,11 @@
 import functools
 
-def trace(recorder):
+def tracer(recorder):
     def wrapper_maker(fn):
         @functools.wraps(fn)
         def fn_wrapper(*args, **kwargs):
             print('START')
             ret_value = fn(*args, **kwargs)
-            # print(ret_value)
             print('END')
             return ret_value
         return fn_wrapper
