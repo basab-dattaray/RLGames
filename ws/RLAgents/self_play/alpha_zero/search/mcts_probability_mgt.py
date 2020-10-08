a2d7c9ad3a0fcad43fa6a563eafacc175cde0e33
basab-dattaray <basab.dattaray@gmail.com>
2020-10-08 10:04:05 -0700

    mac 1.9.3:

---
+++ ws/RLAgents/self_play/alpha_zero/search/mcts_probability_mgt.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def mcts_adapter_mgt(fn_search, fn_get_counts, num_simulations):
+
+    def fn_getActionProb(canonicalBoard, temp=1):
+        temp = 0
+
+        for i in range(num_simulations):
+            fn_search(canonicalBoard)
+
+        counts = fn_get_counts(canonicalBoard)
+
+        if temp == 0:
+            return fn_mcts_probability_select_one_win(counts)
+
+        return fn_mcts_probability_spread_out(counts)
+
+
+    def fn_mcts_probability_spread_out(counts):
+        counts = [x ** (1. / 1) for x in counts]
+        counts_sum = float(sum(counts))
+        probs = [x / counts_sum for x in counts]
+        return probs
+
+
+    def fn_mcts_probability_select_one_win(counts):
+        bestAs = np.array(np.argwhere(counts == np.max(counts))).flatten()
+        bestA = np.random.choice(bestAs)
+        probs = [0] * len(counts)
+        probs[bestA] = 1
+        return probs
+
+    return fn_getActionProb
