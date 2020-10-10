24fa70f7a13b615e5ee3bebb18d98b58e4eaaa14
basab-dattaray <basab.dattaray@gmail.com>
2020-10-10 15:00:50 -0700

    mac 1.9.9.2:

--- ws/RLUtils/monitoring/tracing/progress_count_mgt.py
+++ ws/RLUtils/monitoring/tracing/progress_count_mgt.py
@@ -10,10 +10,8 @@ def progress_count_mgt(title, size):
         nonlocal count
         msg = '{}::: {} of {}'.format(title, count, size)
         count += 1
-        # sys.stdout.write(Fore.BLUE + msg)
-        # sys.stdout.flush()
-        print(Fore.BLUE + msg, end='\r', flush= True)
-        print(flush=True)
+        sys.stdout.write("\r" + Fore.BLUE + msg)
+        sys.stdout.flush()
 
     def end_couunt():
         print(Fore.BLACK)
