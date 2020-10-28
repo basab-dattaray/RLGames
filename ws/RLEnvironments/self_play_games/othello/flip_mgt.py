04db65af1118889bfa19be1fe0d190c0e33a490e
basab-dattaray <basab.dattaray@gmail.com>
2020-10-28 08:04:22 -0700

    mac 1.14.1.3: EXPT create flip_mgt

--- ws/RLEnvironments/self_play_games/othello/flip_mgt.py
+++ ws/RLEnvironments/self_play_games/othello/flip_mgt.py
@@ -22,65 +22,58 @@ def fn_scaffold_display_board(pieceS, sizE):
 def fn_find_flippables(pieceS, sizE, origin_positioN):
 
     directionS = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
-    directionS = [(1, 0)]
+    # directionS = [(-1, 0)]
     origin_color = pieceS[origin_positioN[0]][origin_positioN[1]]
-        
+
     def fn_find_directional_flips(direction_):
 
-        def fn_find_next_flip(pos_, direction_):
-            class Match(Enum):
-                found = 1
-                failed = 2
-                search = 3
-
-            def fn_find_flip(pos__, opponent_is_engulfed= False):
-                if pos__ is None:
-                    return Match.failed
-                travel_color = pieceS[pos__[0]][pos__[1]]
-                if not opponent_is_engulfed:
-                    next_pos = fn_get_next_valid_position(pos__)
-                    return fn_find_flip(next_pos, opponent_is_engulfed= True)
-                else:
-                    if travel_color == -origin_color:
-                        next_pos = fn_get_next_valid_position(pos__)
-                        return fn_find_flip(next_pos, opponent_is_engulfed)
-                    if travel_color == origin_color:
-                        return Match.failed # already taken, sorry!
-                    if travel_color == 0:
-                        return Match.found
-
-            def fn_pos_valid(pos__):
-                return ((pos__[0] >= 0) and (pos__[0] < sizE) or (pos__[1] >= 0) and (pos__[1] < sizE))
+        def fn_find_flip(pos_, first_time= True, opponent_is_engulfed= False):
 
             def fn_get_next_valid_position(pos__):
-                def fn_get_next_position(p):
-                    new_pos = p[0] + direction_[0], p[1] + direction_[1]
+                def fn_pos_valid(pos___):
+                    return ((pos___[0] >= 0) and (pos___[0] < sizE) or (pos___[1] >= 0) and (pos___[1] < sizE))
+
+                def fn_get_next_position(pos___):
+                    new_pos = pos___[0] + direction_[0], pos___[1] + direction_[1]
                     return new_pos
 
                 next_pos = fn_get_next_position(pos__)
                 if not fn_pos_valid(next_pos):
                     return None
                 return next_pos
+            # end def fn_get_next_valid_position
 
-            next_pos = pos_
-            match = fn_find_flip(next_pos)
-
-            while match is Match.search:
-                next_pos = fn_get_next_valid_position(next_pos)
-                match = fn_find_flip(next_pos)
-
-            if match is Match.failed:
+            if pos_ is None:
                 return None
-            if match is Match.found:
-                return next_pos
+            travel_color = pieceS[pos_[0]][pos_[1]]
+            if first_time: # Step 1
+                next_pos = fn_get_next_valid_position(pos_)
+                return fn_find_flip(next_pos, first_time= False)
+            else:
+                if not opponent_is_engulfed: # Step 2
+                    if travel_color == -origin_color:
+                        next_pos = fn_get_next_valid_position(pos_)
+                        return fn_find_flip(next_pos, first_time, opponent_is_engulfed= True)
+                    if travel_color == origin_color:
+                        return None # No hope!
+                    if travel_color == 0:
+                        return None # No Hope
+                else:
+                    if travel_color == -origin_color:
+                        next_pos = fn_get_next_valid_position(pos_)
+                        return fn_find_flip(next_pos, first_time, opponent_is_engulfed)
+                    if travel_color == origin_color:
+                        return None # Already taken, sorry no hope
+                    if travel_color == 0:
+                        return pos_
 
-        pos = origin_positioN
-        flip_position = fn_find_next_flip(pos, direction_)
-        return flip_position
+        # end def fn_find_flip
+        return fn_find_flip(origin_positioN)
+
+    # end def fn_find_directional_flips
 
     # flips = [flip for direction in directionS
     #     for flip in fn_find_directional_flips(direction) if flip is not None]
-
     flips = []
     for direction in directionS:
         flip = fn_find_directional_flips(direction)
@@ -93,14 +86,33 @@ def fn_find_flippables(pieceS, sizE, origin_positioN):
 if __name__ == '__main__':
     size = 5
     pieces = fn_scaffold_init_pieces(5)
+
+    fn_scaffold_display_board(pieces, size)
+
+    #--------------------1
+    origin = (1, 2)
     flips = fn_find_flippables(
         pieces,
         size,
-        (1, 2)
+        origin
     )
-    fn_scaffold_display_board(pieces, size)
+    assert flips == [(3, 2), (1, 0)]
+
     print()
-    print('flips: {}'.format(flips))
+    print('origin: {}, flips: {}'.format(origin, flips))
+
+    #--------------------2
+    origin = (2, 1)
+    flips = fn_find_flippables(
+        pieces,
+        size,
+        origin
+    )
+    assert flips == [(0, 1), (2, 3)]
+
+    print()
+    print('origin: {}, flips: {}'.format(origin, flips))
+
 
 
 
