23cd7ccda565242333baa496ee08c8628bfbd9fb
basab-dattaray <basab.dattaray@gmail.com>
2020-11-02 00:14:03 -0800

    mac 1.21: replaced class MCTS with mcts_r_mgr and class Mtcs with mcts_mgr

--- ws/RLAgents/self_play/alpha_zero/search/non_recursive/mcts_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/search/non_recursive/mcts_mgt.py
@@ -4,7 +4,7 @@
 # A explore_exploit_ratio tells the search to put higher emphasis on exploration relative to exploitation.
 # The more evolved board_pieces states (nodes) will have fewer allowable actions (edges)
 import os
-
+from collections import namedtuple
 
 from .Node import Node
 from .state_cache_mgt import state_cache_mgt
@@ -14,56 +14,70 @@ from ..mcts_probability_mgt import mcts_probability_mgt
 MTCS_RESULTS_FILE_NAME = 'mtcs_results.pkl'
 CACHE_RESULTS = False
 
-class mcts_mgt():
+def mcts_mgt(
+        fn_get_state_key,
+        fn_get_next_state,
+        fn_get_canonical_form,
+        fn_predict_action_probablities,
+        fn_get_valid_actions,
+        fn_terminal_state_status,
+        num_mcts_simulations,
+        explore_exploit_ratio,
+        max_num_actions
+):
 
-    def __init__(self,
-                 fn_get_state_key,
-                 fn_get_next_state,
-                 fn_get_canonical_form,
-                 fn_predict_action_probablities,
-                 fn_get_valid_actions,
-                 fn_terminal_state_status,
-                 num_mcts_simulations,
-                 explore_exploit_ratio,
-                 max_num_actions
-                 ):
+    # app_path =  os.getcwd()
+    root_node = None
+    state_cache = None
 
-        self.app_path = os.getcwd()
+    def fn_init_mcts(state):
+        nonlocal root_node, state_cache
+        root_node = None
+        state_cache = state_cache_mgt(fn_get_valid_actions, fn_predict_action_probablities, state)
+        return True
 
-        self.num_mcts_simulations = num_mcts_simulations
-        self.max_num_actions = max_num_actions
-        self.explore_exploit_ratio = explore_exploit_ratio
+    def fn_get_mcts_counts(state):
+        nonlocal root_node
+        def __fn_get_counts():
+            if root_node is None:
+                return None
+            else:
+                childrenNodes = root_node.children_nodes
+                counts = [0] * max_num_actions
+                for key, val in childrenNodes.items():
+                    index = int(key)
+                    counts[index] = val.visits
+                return counts
 
-        self.root_node = None
 
-        self.fn_get_state_key = fn_get_state_key,
-        self.fn_get_next_state = fn_get_next_state,
-        self.fn_get_canonical_form = fn_get_canonical_form,
-        self.fn_predict_action_probablities = fn_predict_action_probablities
-        self.fn_get_valid_actions = fn_get_valid_actions
-        self.fn_terminal_state_status = fn_terminal_state_status
+        for i in range(num_mcts_simulations):
+            fn_execute_monte_carlo_tree_search(state)
+        counts = __fn_get_counts()
+        return counts
 
-        self.state_cache = None
-        self.fn_get_action_probabilities = mcts_probability_mgt(self.fn_init_mcts, self.fn_get_mcts_counts)
-        self.fn_rollout = rollout_mgt(self.state_cache, self.fn_predict_action_probablities, self.fn_terminal_state_status,
-                    fn_get_next_state, fn_get_canonical_form,
-                    multirun=False)
+    fn_get_action_probabilities = mcts_probability_mgt(fn_init_mcts, fn_get_mcts_counts)
+    fn_rollout = rollout_mgt(state_cache, fn_predict_action_probablities, fn_terminal_state_status,
+                fn_get_next_state, fn_get_canonical_form,
+                multirun=False)
 
-    def fn_execute_monte_carlo_tree_search(self, state):
-        if self.root_node is None:
+    def fn_execute_monte_carlo_tree_search(state):
+        nonlocal  root_node
+        if state_cache is None:
+            return None
+        else:
+            if root_node is None:
+                root_node = Node(
+                    state_cache.fn_get_valid_normalized_action_probabilities,
+                    max_num_actions,
+                    explore_exploit_ratio,
 
-            self.root_node = Node(
-                self.state_cache.fn_get_valid_normalized_action_probabilities,
-                self.max_num_actions,
-                self.explore_exploit_ratio,
+                    parent_action=-1,
+                    val=0.0,
+                    parent_node=None,
+                    state= state
+                )
 
-                parent_action=-1,
-                val=0.0,
-                parent_node=None,
-                state= state
-            )
-
-        selected_node = self.root_node.fn_select_from_available_leaf_nodes()
+        selected_node = root_node.fn_select_from_available_leaf_nodes()
 
         if selected_node.fn_is_already_visited():
             selected_node = selected_node.fn_expand_node()
@@ -71,32 +85,13 @@ class mcts_mgt():
                 return None
             pass
 
-        score, terminal_state = self.fn_rollout(selected_node.state)
+        score, terminal_state = fn_rollout(selected_node.state)
 
-        # score, terminal_state = selected_node.fn_rollout()
-
-        # if terminal_state: print(f'*** score={score}')
         value = selected_node.fn_back_propagate(score)
         return value
 
-    def __fn_reset_mcts(self):
-        self.root_node = None
+    mcts_mgr = namedtuple('_', ['fn_get_action_probabilities'])
+    mcts_mgr.fn_get_action_probabilities = fn_get_action_probabilities
 
-    def fn_get_mcts_counts(self, state):
-        def __fn_get_counts():
-            childrenNodes = self.root_node.children_nodes
-            counts = [0] * self.max_num_actions
-            for key, val in childrenNodes.items():
-                index = int(key)
-                counts[index] = val.visits
-            return counts
+    return mcts_mgr
 
-        for i in range(self.num_mcts_simulations):
-            self.fn_execute_monte_carlo_tree_search(state)
-        counts = __fn_get_counts()
-        return counts
-
-    def fn_init_mcts(self, state):
-        self.state_cache = state_cache_mgt(self.fn_get_valid_actions, self.fn_predict_action_probablities, state)
-        self.__fn_reset_mcts()
-        return True
