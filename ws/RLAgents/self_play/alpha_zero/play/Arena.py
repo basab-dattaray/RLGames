ae7b5e37add2b9565a938bb3f5afa85c0dc94ab8
basab-dattaray <basab.dattaray@gmail.com>
2020-11-03 20:10:41 -0800

    mac 1.21.8.1: change Arena to functional

--- ws/RLAgents/self_play/alpha_zero/play/Arena.py
+++ ws/RLAgents/self_play/alpha_zero/play/Arena.py
@@ -1,5 +1,6 @@
 import functools
 import logging
+from collections import namedtuple
 
 from random import random
 
@@ -9,94 +10,109 @@ from ws.RLUtils.monitoring.tracing.progress_count_mgt import progress_count_mgt
 
 log = logging.getLogger(__name__)
 
-class Arena():
+def Arena(player1, player2, game, fn_display=None, msg_recorder = None):
 
-    def __init__(self, player1, player2, game, fn_display=None, msg_recorder = None):
-        self.player1 = player1
-        self.player2 = player2
-        self.game = game
-        self.fn_display = fn_display
-        self.game_num = 0
-        self.msg_recorder = msg_recorder
 
-    def _fn_play_game(self, verbose=False):
+    game_num = 0
 
+    def _fn_play_game(verbose=False):
+        nonlocal game_num
         DEBUG = False
 
-        players = [self.player2, None, self.player1]
+        players = [player2, None, player1]
         cur_player_index = 1
-        pieces = self.game.fn_get_init_board()
+        pieces = game.fn_get_init_board()
         it = 0
 
         break_from_while = False
-        while self.game.fn_get_game_progress_status(pieces, cur_player_index) == 0:
+        while game.fn_get_game_progress_status(pieces, cur_player_index) == 0:
             it += 1
             if verbose:
-                assert self.fn_display
+                assert fn_display
                 print("Turn ", str(it), "Player ", str(cur_player_index))
-                self.fn_display(pieces)
+                fn_display(pieces)
 
-            cur_player = self.player1 if cur_player_index == 1 else self.player2
+            cur_player = player1 if cur_player_index == 1 else player2
 
             # cur_player = players[cur_player_index + 1]
-            action = cur_player(self.game.fn_get_canonical_form(pieces, cur_player_index))
+            action = cur_player(game.fn_get_canonical_form(pieces, cur_player_index))
 
-            valids = self.game.fn_get_valid_moves(self.game.fn_get_canonical_form(pieces, cur_player_index), 1)
+            valids = game.fn_get_valid_moves(game.fn_get_canonical_form(pieces, cur_player_index), 1)
 
             if valids[action] == 0:
                 if DEBUG:
                     x = (int) (action / len(pieces))
                     y = action % len(pieces)
-                    self.msg_recorder(f'Action {action} is not valid!   [{x} {y}]')
+                    msg_recorder(f'Action {action} is not valid!   [{x} {y}]')
 
-                    self.msg_recorder(f'Current Player: {cur_player_index} ')
+                    msg_recorder(f'Current Player: {cur_player_index} ')
 
 
-                    self.msg_recorder(f'valids = {valids}')
-                    self.msg_recorder('')
+                    msg_recorder(f'valids = {valids}')
+                    msg_recorder('')
 
                     for i in range(len(pieces)):
                         # arr_of_strs = map(lambda size: '{0:03d}'.format(size), board_pieces[i])
                         # line = list(arr_of_strs)
                         lst = list(map(lambda n: '0' if n == 0 else '+' if n > 0 else '-', pieces[i]))
                         line = functools.reduce(lambda a,b : a + ' ' + b,lst)
-                        self.msg_recorder(line)
+                        msg_recorder(line)
                 break_from_while = True
                 break
 
                 # log.debug(f'valids = {valids}')
                 # assert valids[action] > 0
-            pieces, cur_player_index = self.game.fn_get_next_state(pieces, cur_player_index, action)
+            pieces, cur_player_index = game.fn_get_next_state(pieces, cur_player_index, action)
 
-        game_status = self.game.fn_game_status(pieces)
+        game_status = game.fn_game_status(pieces)
 
         if verbose:
-            assert self.fn_display
+            assert fn_display
             print("Game over: Turn ", str(it), "Result ", str(game_status))
-            self.fn_display(pieces)
+            fn_display(pieces)
 
 
         result = game_status
         if DEBUG:
             color = Fore.RED
-            game_status1 = self.game.fn_get_game_progress_status(pieces, cur_player_index)
+            game_status1 = game.fn_get_game_progress_status(pieces, cur_player_index)
 
             result1 = cur_player_index * game_status1
             if result == result1:
                 color = Fore.GREEN
 
-            self.msg_recorder(color + f'curPlayer= {cur_player_index}')
-            self.msg_recorder(f'RESULT:: {result}')
-            self.msg_recorder(f'RESULT1:: {result1}  --> old')
+            msg_recorder(color + f'curPlayer= {cur_player_index}')
+            msg_recorder(f'RESULT:: {result}')
+            msg_recorder(f'RESULT1:: {result1}  --> old')
 
-            self.msg_recorder(Fore.BLUE)
-        # print(f'Game number={self.game_num}; curPlayer={curPlayer}; result={result}')
-        self.game_num += 1
+            msg_recorder(Fore.BLUE)
+        # print(f'Game number={game_num}; curPlayer={curPlayer}; result={result}')
+        game_num += 1
         return result
 
 
-    def fn_play_games(self, num_of_games, verbose=False):
-        self.count_episode, self.end_couunting = progress_count_mgt('Game Counts', num_of_games)
+    def fn_play_games(num_of_games, verbose=False):
+        def _fn_get_gameset_results(num, result_factor, verbose):
+            oneWon = 0
+            twoWon = 0
+            draws = 0
+            for i in range(num):
+                count_episode()
+                gameResult = _fn_play_game(verbose=verbose)
+
+                if gameResult == 1 * result_factor:
+                    oneWon += 1
+                elif gameResult == -1 * result_factor:
+                    twoWon += 1
+                else:
+                    draws += 1
+
+            return oneWon, twoWon, draws
+
+        count_episode, end_couunting = progress_count_mgt('Game Counts', num_of_games)
+
+        nonlocal player1, player2
+
         num_div_2 = int(num_of_games / 2)
         extra_for_1 = 0
         extra_for_2 = 0
@@ -106,29 +122,18 @@ class Arena():
             else:
                 extra_for_2 = 1
 
-        oneWon_1, twoWon_1, draws_1 = self._fn_get_gameset_results(num_div_2 + extra_for_1, 1, verbose)
-        self.player1, self.player2 = self.player2, self.player1
-        oneWon_2, twoWon_2, draws_2 = self._fn_get_gameset_results(num_div_2 + extra_for_2, -1, verbose)
+        oneWon_1, twoWon_1, draws_1 = _fn_get_gameset_results(num_div_2 + extra_for_1, 1, verbose)
+        player1, player2 = player2, player1
+        oneWon_2, twoWon_2, draws_2 = _fn_get_gameset_results(num_div_2 + extra_for_2, -1, verbose)
 
-        self.end_couunting()
+        end_couunting()
         return oneWon_1 + oneWon_2, twoWon_1 + twoWon_2, draws_1 + draws_2
 
-    def _fn_get_gameset_results(self, num, result_factor, verbose):
-        oneWon = 0
-        twoWon = 0
-        draws = 0
-        for i in range(num):
-            self.count_episode()
-            gameResult = self._fn_play_game(verbose=verbose)
+    playground_mgr = namedtuple('_', ['fn_play_games'])
+    playground_mgr.fn_get_action_probabilities=fn_play_games
 
-            if gameResult == 1 * result_factor:
-                oneWon += 1
-            elif gameResult == -1 * result_factor:
-                twoWon += 1
-            else:
-                draws += 1
+    return playground_mgr
 
-        return oneWon, twoWon, draws
 
 
 
