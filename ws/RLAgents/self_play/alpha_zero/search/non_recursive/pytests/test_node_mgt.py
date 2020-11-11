b39b3fc201368f072b990e4d03085e94656625dc
basab-dattaray <basab.dattaray@gmail.com>
2020-11-10 17:00:18 -0800

    mac 1.26.18:

--- ws/RLAgents/self_play/alpha_zero/search/non_recursive/pytests/test_node_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/search/non_recursive/pytests/test_node_mgt.py
@@ -1,31 +1,36 @@
 import pytest
 
+from ws.RLAgents.self_play.alpha_zero.misc.agent_mgt import agent_mgt
+from ws.RLAgents.self_play.alpha_zero.search.non_recursive.pytests.ARGS import args
+# from ..node_mgt import node_mgt
+# from ...mcts_adapter import mcts_adapter
 # from ws.RLEnvironments.self_play_games.othello.game_mgt import game_mgt
-from ws.RLAgents.model_free.policy_gradient.agent_mgt import agent_mgt
-from ..node_mgt import node_mgt
-from ...mcts_adapter import mcts_adapter
-from ws.RLEnvironments.self_play_games.othello.game_mgt import game_mgt
 
 GAME_SIZE= 5
 
-def init():
-    from ws.RLUtils.monitoring.tracing.trace_example.agent_caller import args
+@pytest.fixture()
+def init_agent():
+
     agent = agent_mgt(args, __file__)
-    game = game_mgt(GAME_SIZE)
-    pmcts = mcts_adapter(nn_mgr_P, args)
+    return agent
 
-def test_create_root_node():
 
-    root_node = node_mgt(
-        state,
-        fn_get_normalized_predictions,
-        max_num_actions,
-        explore_exploit_ratio,
+def test_create_root_node(init_agent):
+    # agent = init_agent(args, __file__)
+    args = init_agent.arguments
 
-        parent_action=-1,
-        val=0.0,
-        parent_node=None
-    )
+    x = 3
+
+    # root_node = node_mgt(
+    #     state,
+    #     fn_get_normalized_predictions,
+    #     max_num_actions,
+    #     explore_exploit_ratio,
+    #
+    #     parent_action=-1,
+    #     val=0.0,
+    #     parent_node=None
+    # )
 
 
 
