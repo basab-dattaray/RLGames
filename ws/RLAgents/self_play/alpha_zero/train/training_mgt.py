44bfd57e159909ad307c5d44b8a33c60f029b0c1
basab-dattaray <basab.dattaray@gmail.com>
2020-11-13 18:38:45 -0800

    mac 1.28.1.2:

--- ws/RLAgents/self_play/alpha_zero/train/training_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/train/training_mgt.py
@@ -11,6 +11,7 @@ from pip._vendor.colorama import Fore
 
 from ws.RLAgents.self_play.alpha_zero.play.playground_mgt import playground_mgt
 from ws.RLAgents.self_play.alpha_zero.search.mcts_adapter import mcts_adapter
+from ws.RLAgents.self_play.alpha_zero.train.sample_generator import fn_generate_samples
 from ws.RLAgents.self_play.alpha_zero.train.training_helper import fn_save_train_examples, fn_log_iter_results, \
     fn_getCheckpointFile
 
@@ -29,7 +30,7 @@ def training_mgt(nn_mgr_N, args):
 
 
 
-    training_samples_buffer = []  # history of examples from nn_args.sample_history_buffer_size latest iterations
+    # training_samples_buffer = []  # history of examples from nn_args.sample_history_buffer_size latest iterations
     # skipFirstSelfPlay = False  # can be overriden in loadTrainExamples()
 
 
@@ -48,79 +49,79 @@ def training_mgt(nn_mgr_N, args):
         @tracer(args)
         def fn_run_iteration(iteration):
             nonlocal update_count
-            @tracer(args)
-            def fn_generate_samples(args, iteration, generation_mcts):
-
-
-                def _fn_run_episodes():
-                    trainExamples = []
-                    current_pieces = game_mgr.fn_get_init_board()
-                    curPlayer = 1
-                    episode_step = 0
-
-                    def _fn_run_one_episode(trainExamples, current_pieces, curPlayer, episode_step):
-                        canonical_board_pieces = game_mgr.fn_get_canonical_form(current_pieces, curPlayer)
-                        spread_probabilities = int(episode_step < args.probability_spread_threshold)
-
-                        action_probs = generation_mcts.fn_get_action_probabilities(canonical_board_pieces,
-                                                                                   spread_probabilities=spread_probabilities)
-                        if action_probs is None:
-                            return None
-
-                        symetric_samples = game_mgr.fn_get_symetric_samples(canonical_board_pieces, action_probs)
-                        # trainExamples = map(lambda b, p: trainExamples.append([b, curPlayer, p, None]), sym)
-                        for sym_canon_board, canon_action_probs in symetric_samples:
-                            trainExamples.append((sym_canon_board, curPlayer, canon_action_probs))
-
-                        action = np.random.choice(len(action_probs), p=action_probs)
-                        next_pieces, player_next = game_mgr.fn_get_next_state(current_pieces, curPlayer, action)
-
-                        if DEBUG_FLAG:
-                            print()
-                            print('player:{}'.format(curPlayer))
-                            print()
-                            print(next_pieces)
-
-                        curPlayer = player_next
-                        current_pieces = next_pieces
-                        return trainExamples, current_pieces, curPlayer
-
-                    while True:
-                        episode_step += 1
-
-                        trainExamples, current_pieces, curPlayer = _fn_run_one_episode(trainExamples, current_pieces, curPlayer, episode_step)
-                        game_status = game_mgr.fn_get_game_progress_status(current_pieces, curPlayer)
-
-                        if game_status != 0 or curPlayer is None:
-                            return _fn_form_sample_data(curPlayer, game_status, trainExamples)
-
-                # examples of the iteration
-                if iteration > 1:
-                    samples_for_iteration = deque([], maxlen=args.sample_buffer_size)
-                    fn_count_event, fn_stop_counting = progress_count_mgt('Episodes', args.num_of_training_episodes)
-                    for episode_num in range(1, args.num_of_training_episodes + 1):
-                        fn_count_event()
-                        episode_result = _fn_run_episodes()
-                        if episode_result is not None:
-                            samples_for_iteration += episode_result
-                    fn_stop_counting()
-                    args.calltracer.fn_write(f'Number of Episodes for sample generation: {args.num_of_training_episodes}')
-
-                    # save the iteration examples to the history
-                    training_samples_buffer.append(samples_for_iteration)
-                if len(training_samples_buffer) > args.sample_history_buffer_size:
-                    args.logger.warning(
-                        f"Removing the oldest entry in trainExamples. len(training_samples_buffer) = {len(training_samples_buffer)}")
-                    training_samples_buffer.pop(0)
-                # backup history to a file
-                # NB! the examples were collected using the model from the previous iteration, so (iteration-1)
-                fn_save_train_examples(args, iteration - 1, training_samples_buffer)
-                # shuffle examples before training
-                trainExamples = []
-                for e in training_samples_buffer:
-                    trainExamples.extend(e)
-                shuffle(trainExamples)
-                return trainExamples
+            # @tracer(args)
+            # def fn_generate_samples(args, iteration, generation_mcts):
+            #
+            #
+            #     def _fn_run_episodes():
+            #         trainExamples = []
+            #         current_pieces = game_mgr.fn_get_init_board()
+            #         curPlayer = 1
+            #         episode_step = 0
+            #
+            #         def _fn_run_one_episode(trainExamples, current_pieces, curPlayer, episode_step):
+            #             canonical_board_pieces = game_mgr.fn_get_canonical_form(current_pieces, curPlayer)
+            #             spread_probabilities = int(episode_step < args.probability_spread_threshold)
+            #
+            #             action_probs = generation_mcts.fn_get_action_probabilities(canonical_board_pieces,
+            #                                                                        spread_probabilities=spread_probabilities)
+            #             if action_probs is None:
+            #                 return None
+            #
+            #             symetric_samples = game_mgr.fn_get_symetric_samples(canonical_board_pieces, action_probs)
+            #             # trainExamples = map(lambda b, p: trainExamples.append([b, curPlayer, p, None]), sym)
+            #             for sym_canon_board, canon_action_probs in symetric_samples:
+            #                 trainExamples.append((sym_canon_board, curPlayer, canon_action_probs))
+            #
+            #             action = np.random.choice(len(action_probs), p=action_probs)
+            #             next_pieces, player_next = game_mgr.fn_get_next_state(current_pieces, curPlayer, action)
+            #
+            #             if DEBUG_FLAG:
+            #                 print()
+            #                 print('player:{}'.format(curPlayer))
+            #                 print()
+            #                 print(next_pieces)
+            #
+            #             curPlayer = player_next
+            #             current_pieces = next_pieces
+            #             return trainExamples, current_pieces, curPlayer
+            #
+            #         while True:
+            #             episode_step += 1
+            #
+            #             trainExamples, current_pieces, curPlayer = _fn_run_one_episode(trainExamples, current_pieces, curPlayer, episode_step)
+            #             game_status = game_mgr.fn_get_game_progress_status(current_pieces, curPlayer)
+            #
+            #             if game_status != 0 or curPlayer is None:
+            #                 return _fn_form_sample_data(curPlayer, game_status, trainExamples)
+            #
+            #     # examples of the iteration
+            #     if iteration > 1:
+            #         samples_for_iteration = deque([], maxlen=args.sample_buffer_size)
+            #         fn_count_event, fn_stop_counting = progress_count_mgt('Episodes', args.num_of_training_episodes)
+            #         for episode_num in range(1, args.num_of_training_episodes + 1):
+            #             fn_count_event()
+            #             episode_result = _fn_run_episodes()
+            #             if episode_result is not None:
+            #                 samples_for_iteration += episode_result
+            #         fn_stop_counting()
+            #         args.calltracer.fn_write(f'Number of Episodes for sample generation: {args.num_of_training_episodes}')
+            #
+            #         # save the iteration examples to the history
+            #         training_samples_buffer.append(samples_for_iteration)
+            #     if len(training_samples_buffer) > args.sample_history_buffer_size:
+            #         args.logger.warning(
+            #             f"Removing the oldest entry in trainExamples. len(training_samples_buffer) = {len(training_samples_buffer)}")
+            #         training_samples_buffer.pop(0)
+            #     # backup history to a file
+            #     # NB! the examples were collected using the model from the previous iteration, so (iteration-1)
+            #     fn_save_train_examples(args, iteration - 1, training_samples_buffer)
+            #     # shuffle examples before training
+            #     trainExamples = []
+            #     for e in training_samples_buffer:
+            #         trainExamples.extend(e)
+            #     shuffle(trainExamples)
+            #     return trainExamples
 
             @tracer(args)
             def _fn_play_next_vs_previous(trainExamples):
