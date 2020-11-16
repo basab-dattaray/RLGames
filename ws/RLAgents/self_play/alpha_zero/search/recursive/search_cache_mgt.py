2bda15f574baf14878d7c97929848603c363cf59
basab-dattaray <basab.dattaray@gmail.com>
2020-11-15 21:55:58 -0800

    mac 1.29.3.1:

--- ws/RLAgents/self_play/alpha_zero/search/recursive/search_cache_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/search/recursive/search_cache_mgt.py
@@ -2,21 +2,21 @@ from collections import namedtuple
 
 
 def search_cache_mgt():
-    Es = {}
+    Es = {}     # stores game.fn_get_game_progress_status ended for board_pieces state
 
-    def does_end_exist(state_key):
+    def fn_does_end_exist(state_key):
         return state_key in Es
 
-    def get_end_state(state_key):
-        if does_end_exist(state_key):
+    def fn_get_end_state(state_key):
+        if fn_does_end_exist(state_key):
             return Es[state_key]
         else:
             return None
 
 
-    def set_end_state(state_key, val):
+    def fn_set_end_state(state_key, val):
         nonlocal Es
-        if does_end_exist(state_key):
+        if fn_does_end_exist(state_key):
             return False
         else:
             Es[state_key] = val
@@ -27,8 +27,8 @@ def search_cache_mgt():
         'fn_get_end_state',
         'fn_set_end_state'
     ])
-    search_cache_mgr.fn_does_end_exist = does_end_exist
-    search_cache_mgr.fn_get_end_state = get_end_state
-    search_cache_mgr.fn_set_end_state = set_end_state
+    search_cache_mgr.fn_does_end_exist = fn_does_end_exist
+    search_cache_mgr.fn_get_end_state = fn_get_end_state
+    search_cache_mgr.fn_set_end_state = fn_set_end_state
 
     return search_cache_mgr
