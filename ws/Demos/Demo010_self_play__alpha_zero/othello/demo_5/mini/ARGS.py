cd982a79c72037027e7d77a618c63ee9cfa592c6
basab-dattaray <basab.dattaray@gmail.com>
2020-12-01 15:58:47 -0800

    1.34.4.3 mac: streamlined mcts_adapter

--- ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini/ARGS.py
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini/ARGS.py
@@ -3,7 +3,7 @@ from ws.RLUtils.common.DotDict import DotDict
 args = DotDict({
     'num_of_training_iterations': 3,
     'num_of_training_episodes': 3,              # Number of complete self-play games to simulate during a new iteration.
-    'probability_spread_threshold': 0, # 15,        #
+    'probability_spread_threshold': 15,        #
     'score_based_model_update_threshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'sample_buffer_size': 200000,    # Number of game examples to train the neural networks.
     'num_of_mc_simulations': 5,          # Number of games moves for MCTS to simulate.
