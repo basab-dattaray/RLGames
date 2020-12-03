5b679f30df2ff770b669150f309479a61b636c01
basab-dattaray <basab.dattaray@gmail.com>
2020-12-03 10:43:29 -0800

    1.36.3.2 mac:

--- ws/RLAgents/self_play/alpha_zero/play/playground_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/play/playground_mgt.py
@@ -1,19 +1,21 @@
-import functools
-import logging
+
 from collections import namedtuple
 
 from random import random
 
-from pip._vendor.colorama import Fore
-
 from ws.RLUtils.monitoring.tracing.progress_count_mgt import progress_count_mgt
 
-# log = logging.getLogger(__name__)
 
-def playground_mgt(player1, player2, game_mgr, fn_display=None, msg_recorder = None):
+def playground_mgt(fn_policy_player1, fn_policy_player2, game_mgr, fn_display=None, msg_recorder = None):
     game_num = 0
 
+
+
     def _fn_play_game(verbose=False):
+        def _fn_switch_policy(cur_player_index):
+            curent_policy = fn_policy_player1 if cur_player_index == 1 else fn_policy_player2
+            return curent_policy
+
         def _fn_display_if_verbose(verbose):
             if verbose:
                 assert fn_display
@@ -22,7 +24,6 @@ def playground_mgt(player1, player2, game_mgr, fn_display=None, msg_recorder = N
                 fn_display(pieces)
 
         nonlocal game_num
-        DEBUG = False
 
         cur_player_index = 1
         pieces = game_mgr.fn_get_init_board()
@@ -32,17 +33,18 @@ def playground_mgt(player1, player2, game_mgr, fn_display=None, msg_recorder = N
             loop_count += 1
             _fn_display_if_verbose(verbose)
 
-            cur_player = player1 if cur_player_index == 1 else player2
+            curent_policy = _fn_switch_policy(cur_player_index)
 
-            # cur_player = players[cur_player_index + 1]
-            action = cur_player(game_mgr.fn_get_canonical_form(pieces, cur_player_index))
-            if action == None:
-                break
-            valid_moves = game_mgr.fn_get_valid_moves(game_mgr.fn_get_canonical_form(pieces, cur_player_index), 1)
+            canonical_pieces = game_mgr.fn_get_canonical_form(pieces, cur_player_index)
+
+
+            valid_moves = game_mgr.fn_get_valid_moves(canonical_pieces, 1)
             if valid_moves is None:
                 break
-            # if valid_moves[action] == 0:
-            #      break
+
+            action = curent_policy(canonical_pieces)
+            if action == None:
+                break
 
             pieces, cur_player_index = game_mgr.fn_get_next_state(pieces, cur_player_index, action)
 
@@ -57,7 +59,7 @@ def playground_mgt(player1, player2, game_mgr, fn_display=None, msg_recorder = N
 
 
     def fn_play_games(num_of_games, verbose=False):
-        nonlocal player1, player2
+        nonlocal fn_policy_player1, fn_policy_player2
 
         def _fn_get_gameset_results(num, result_factor, verbose):
             oneWon = 0
@@ -88,7 +90,7 @@ def playground_mgt(player1, player2, game_mgr, fn_display=None, msg_recorder = N
                 extra_for_2 = 1
 
         oneWon_1, twoWon_1, draws_1 = _fn_get_gameset_results(num_div_2 + extra_for_1, 1, verbose)
-        player1, player2 = player2, player1
+        fn_policy_player1, fn_policy_player2 = fn_policy_player2, fn_policy_player1
         oneWon_2, twoWon_2, draws_2 = _fn_get_gameset_results(num_div_2 + extra_for_2, -1, verbose)
 
         fn_stop_counting()
