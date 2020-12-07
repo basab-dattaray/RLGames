7f2c4fd0df3dafa0a2cf36bc604545dced053dd2
basab-dattaray <basab.dattaray@gmail.com>
2020-12-07 12:25:56 -0800

    1.37.3.2 mac: test

---
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/full_with_recursive_mcts/demo_train_compare.py
@@ -0,0 +1,43 @@
+
+import coloredlogs
+
+from ws.Demos.Demo010_self_play__alpha_zero.othello.demo_5.mini_with_recursive_mcts.ARGS import args
+# from ws.RLAgents.self_play.alpha_zero.misc.Agent import Agent
+from ws.RLAgents.self_play.alpha_zero.misc.agent_mgt import agent_mgt
+
+coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
+
+if __name__ == "__main__":
+    try:
+
+        agent_mgt(args, __file__). \
+            fn_change_args({
+                'run_recursive_search': 1,
+                'do_load_model': False,
+                'mcts_ucb_use_action_prob_for_exploration': True,
+            }). \
+            fn_train().\
+            fn_change_args({
+                'num_of_mc_simulations': 50,
+            }). \
+            fn_show_args(). \
+            fn_test_against_greedy(). \
+            fn_test_against_random(). \
+            fn_measure_time_elapsed(). \
+            \
+        fn_change_args({
+            'run_recursive_search': 1,
+            'do_load_model': False,
+            'mcts_ucb_use_action_prob_for_exploration': False,
+        }). \
+            fn_train(). \
+            fn_change_args({
+            'num_of_mc_simulations': 50,
+        }). \
+            fn_show_args(). \
+            fn_test_against_greedy(). \
+            fn_test_against_random(). \
+            fn_measure_time_elapsed(). \
+            fn_archive_log_file()
+    except Exception as x:
+        print(f'*** DEMO ---{x}')
