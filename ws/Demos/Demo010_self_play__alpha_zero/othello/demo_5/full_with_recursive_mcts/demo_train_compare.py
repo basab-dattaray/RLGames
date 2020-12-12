20f5acc575ff6059db021c721e014c37508a7073
basab-dattaray <basab.dattaray@gmail.com>
2020-12-11 17:43:34 -0800

    1.37.4.8.5 mac: 3 compares

--- ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/full_with_recursive_mcts/demo_train_compare.py
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/full_with_recursive_mcts/demo_train_compare.py
@@ -15,6 +15,18 @@ if __name__ == "__main__":
             fn_reset(). \
             fn_change_args({
                 'do_load_model': False,
+                'mcts_ucb_use_action_prob_for_exploration': True,
+                'mcts_ucb_use_log_in_numerator': True,
+            }). \
+            fn_train(). \
+            fn_test_against_greedy(). \
+            fn_test_against_random(). \
+            fn_measure_time_elapsed(). \
+            fn_archive_log_file(). \
+            \
+            fn_reset(). \
+            fn_change_args({
+                'do_load_model': False,
                 'mcts_ucb_use_action_prob_for_exploration': False,
                 'mcts_ucb_use_log_in_numerator': False,
             }). \
