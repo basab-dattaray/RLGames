4cb8f581223adbb844566c4f6181334c10ab0529
basab-dattaray <basab.dattaray@gmail.com>
2020-12-28 11:41:05 -0800

    2.1.9.2 mac:

--- ws/RLAgents/self_play/alpha_zero/search/recursive/hier_cache_mgt.py
+++ ws/RLAgents/self_play/alpha_zero/search/recursive/hier_cache_mgt.py
@@ -2,8 +2,8 @@ from collections import namedtuple
 from ws.RLAgents.self_play.alpha_zero.search.recursive.dict_cache import dict_cache
 
 def hier_cache_mgt():
-    s_results = dict_cache()
-    s_allowed_moves = dict_cache()
+    # s_results = dict_cache()
+    # s_allowed_moves = dict_cache()
     s_info = dict_cache()
 
     sa_qval = dict_cache()
@@ -11,13 +11,13 @@ def hier_cache_mgt():
     ret_obj = namedtuple('_', [
         # 's_results',
         's_info',
-        's_allowed_moves',
+        # 's_allowed_moves',
 
         'sa_qval',
 
     ])
     # ret_obj.s_results = s_results
-    ret_obj.s_allowed_moves = s_allowed_moves
+    # ret_obj.s_allowed_moves = s_allowed_moves
     ret_obj.s_info = s_info
 
     ret_obj.sa_qval = sa_qval
