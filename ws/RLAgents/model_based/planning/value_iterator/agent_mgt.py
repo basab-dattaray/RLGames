6c340c5e749214969a91827f0e770ac72fc2cf7c
basab-dattaray <basab.dattaray@gmail.com>
2021-01-01 14:18:38 -0800

    2.3.2.3 mac:

--- ws/RLAgents/model_based/planning/value_iterator/agent_mgt.py
+++ ws/RLAgents/model_based/planning/value_iterator/agent_mgt.py
@@ -1,9 +1,15 @@
+from collections import OrderedDict
+
 from ws.RLAgents.model_based.planning.impl_mgt import impl_mgt
 
 
 def agent_mgt(app_info, env):
     def fn_init():
         fn_bind_fn_display_actions, fn_move_per_policy, fn_apply_policy_iteration, fn_apply_value_iteration = impl_mgt(env, app_info)
-        fn_bind_fn_display_actions({"plan": fn_apply_value_iteration, "move": fn_move_per_policy})
+        actions = OrderedDict()
+        actions["plan"] = fn_apply_value_iteration
+        actions["move"] = fn_move_per_policy
+
+        fn_bind_fn_display_actions(actions) #{"plan": fn_apply_value_iteration, "move": fn_move_per_policy})
 
     return fn_init
