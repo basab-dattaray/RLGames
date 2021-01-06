1c2502fcd1b2ccab51fdb420be34e467e5405585
basab-dattaray <basab.dattaray@gmail.com>
2021-01-05 16:41:54 -0800

    2.14.6 mac:

--- ws/RLUtils/setup/preparation_mgt.py
+++ ws/RLUtils/setup/preparation_mgt.py
@@ -25,6 +25,16 @@ def fn_set_app_info_paths(_app_info, calling_root_path):
     root_dot_path = relative_package_path.replace('/', '.')
     _app_info['ROOT_DOT_PATH'] = root_dot_path
 
+def fn_gpu_setup(_app_info, verbose= False):
+    # GPU
+    _app_info['GPU_DEVICE'] = get_device(_app_info)
+    if verbose:
+        print('DEVICE: {}'.format(_app_info['GPU_DEVICE']))
+
+    agent_config = _app_info['AGENT_CONFIG']
+        # load_function(function_name="agent_mgt", module_tag="agent_mgt", subpackage_tag=subpackage_name)
+    pass
+
 def preparation_mgt(calling_filepath, verbose=False):
     filepathname_parts = calling_filepath.rsplit('/', 1)
     cwd = filepathname_parts[0]
@@ -36,6 +46,8 @@ def preparation_mgt(calling_filepath, verbose=False):
     _app_info_path = os.path.join(cwd, app_info_file)
     _app_info = get_json_data(_app_info_path)
     _app_info['APP_INFO_SOURCE'] = _app_info_path
+
+
 
     def _fn_setup_for_results():
         _app_info['DEMO_PATH'] = cwd
@@ -97,20 +109,10 @@ def preparation_mgt(calling_filepath, verbose=False):
 
         return env
 
-    def fn_misc_setup(_app_info):
-        # GPU
-        _app_info['GPU_DEVICE'] = get_device(_app_info)
-        if verbose:
-            print('DEVICE: {}'.format(_app_info['GPU_DEVICE']))
-
-        agent_config = _app_info['AGENT_CONFIG']
-            # load_function(function_name="agent_mgt", module_tag="agent_mgt", subpackage_tag=subpackage_name)
-        pass
-
     _fn_setup_for_results()
     _fn_setup_paths_in_app_info()
     _fn_setup_logging()
-    fn_misc_setup(_app_info)
+    fn_gpu_setup(_app_info)
 
     env = _fn_get_env()
 
