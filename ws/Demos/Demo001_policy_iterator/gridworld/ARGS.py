7e17c1f1ea57a539428504fb57708bbe11a2df45
basab-dattaray <basab.dattaray@gmail.com>
2021-01-07 20:05:47 -0800

    2.18.5.1 mac:  added startup_mgt

--- ws/Demos/Demo001_policy_iterator/gridworld/ARGS.py
+++ ws/Demos/Demo001_policy_iterator/gridworld/ARGS.py
@@ -1,29 +1,26 @@
 from ws.RLUtils.common.DotDict import DotDict
 
 args = DotDict({
-    'num_of_training_iterations': 3,
-    'num_of_training_episodes': 3,              # Number of complete self-play games to simulate during a new iteration.
-    'probability_spread_threshold': 15,        #
-    'score_based_model_update_threshold': 0.5,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'sample_buffer_size': 200000,    # Number of game examples to train the neural networks.
-    'num_of_mc_simulations': 27,          # Number of games moves for MCTS to simulate.
-    'number_of_games_for_model_comarison': 4,         # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct_exploration_exploitation_factor': 1,
+    "STRATEGY": "CAT1_model_based.planning.policy_iterator",
+    "ENV_NAME": "Gridworld-v1",
+    "AGENT_CONFIG": "gridwell_1",
 
-    'do_load_model': True,
-    # 'rel_model_path': 'tmp/',
-    # 'do_load_samples': False,
-    # 'model_name':'model.tar',
-    # 'load_folder_file': ('tmp/','model.tar'),
-    'sample_history_buffer_size': 20,
+    "NUM_EPISODES": 1,
+    "BATCH_SIZE": 64,
+    "GAMMA": 0.99,
+    "LEARNING_RATE": 0.01,
+    "EPSILON": 0.1,
+    "RHO": 0.99,
+    "DISCOUNT_FACTOR": 0.9,
 
-    'epochs': 4,
-    'board_size': 5,
-    'num_of_test_games': 8,
-
-    'mcts_ucb_use_log_in_numerator': True,
-    'mcts_ucb_use_action_prob_for_exploration': True,
-
+    "display": {
+        "APP_NAME": "Policy Iterator",
+        "BOARD_BLOCKERS": [{"x": 1, "y": 2, "reward": -100}, {"x": 2, "y": 1, "reward": -100}],
+        "BOARD_GOAL": {"x": 2, "y": 2, "reward": 100},
+        "UNIT": 100,
+        "WIDTH": 6,
+        "HEIGHT": 5
+    }
 })
 
 def fn_get_args():
