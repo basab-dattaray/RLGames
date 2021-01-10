551af182d138ced3b1755cd4642a5236f0152d25
basab-dattaray <basab.dattaray@gmail.com>
2021-01-10 11:49:47 -0800

    2.20.2 mac:

--- ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/ARGS.py
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/ARGS.py
@@ -3,12 +3,12 @@ from ws.RLUtils.common.DotDict import DotDict
 args = DotDict({
     'STRATEGY': 'CAT4_self_play.alpha_zero',
     'NUM_TRAINING_ITERATIONS': 3,
-    'NUM_TRAINING_EPISODES': 3,              # Number of complete self-play games to simulate during a new iteration.
-    'PROBABILITY_SPREAD_THRESHOLD': 15,        #
-    'SCORE_BASED_MODEL_UPDATE_THRESHOLD': 0.5,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'SAMPLE_BUFFER_SIZE': 200000,    # Number of game examples to train the neural networks.
-    'NUM_MC_SIMULATIONS': 27,          # Number of games moves for MCTS to simulate.
-    'NUM_GAMES_FOR_MODEL_COMPARISON': 4,         # Number of games to play during arena play to determine if new net will be accepted.
+    'NUM_TRAINING_EPISODES': 3,
+    'PROBABILITY_SPREAD_THRESHOLD': 15,
+    'SCORE_BASED_MODEL_UPDATE_THRESHOLD': 0.5,
+    'SAMPLE_BUFFER_SIZE': 200000,
+    'NUM_MC_SIMULATIONS': 27,
+    'NUM_GAMES_FOR_MODEL_COMPARISON': 4,
     'EXPLORE_EXPLOIT_FACTOR': 1,
 
     'DO_LOAD_MODEL': True,
