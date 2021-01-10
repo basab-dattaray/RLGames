0b4d1e32c84b4e7a0ff9d11266b3279a02575d23
basab-dattaray <basab.dattaray@gmail.com>
2021-01-09 22:16:52 -0800

    2.19.1 mac:

--- ws/Demos/Demo006_dqn/lunarlander/demo_run.py
+++ ws/Demos/Demo006_dqn/lunarlander/demo_run.py
@@ -1,23 +1,5 @@
-# from ws.RLInterfaces.PARAM_KEY_NAMES import STRATEGY
 from ws.RLAgents.CAT2_value_based.fn_approx_yes__off_policy__bootstrap.dqn.agent_mgt import agent_mgt
-from ws.RLUtils.setup.preparation_mgt import preparation_mgt
-# from ws.RLUtils.common.module_loader import load_function
-
-import os
 
 if __name__ == "__main__":
-    app_info, env = preparation_mgt(__file__)
-    subpackage_name = 'ws.RLAgents.{}'.format(app_info['STRATEGY'])
-
-    # agent_mgt = load_function(function_name="agent_mgt", module_tag="agent_mgt", subpackage_tag=subpackage_name)
-
-    fnTrain, fnSaveWeights, fnLoadWeights = agent_mgt(app_info, env)
-
-    cwd = __file__.rsplit('/', 1)[0]
-    model_dir = os.path.join(cwd, "models")
-    fnLoadWeights(model_dir)
-    fnTrain()
-
-    fnSaveWeights(model_dir)
-    env.fn_close()
+    agent_mgt(__file__)
 
--- ws/RLAgents/CAT2_value_based/fn_approx_yes__off_policy__bootstrap/dqn/agent_mgt.py
+++ ws/RLAgents/CAT2_value_based/fn_approx_yes__off_policy__bootstrap/dqn/agent_mgt.py
@@ -1,8 +1,12 @@
+import os
+
 import numpy as np
 
+from ws.RLUtils.setup.preparation_mgt import preparation_mgt
 from .impl_mgt import impl_mgt
 
-def agent_mgt(app_info, env):
+def agent_mgt(caller_file):
+    app_info, env = preparation_mgt(caller_file)
     _action_size = env.fn_get_action_size()
     _state_size = env.fn_get_state_size()
 
@@ -11,32 +15,44 @@ def agent_mgt(app_info, env):
     def fnTrain():
         # nonlocal fn_remember
 
-        num_episodes = app_info['NUM_EPISODES']
+        # num_episodes = app_info['NUM_EPISODES']
         loss = []
-        for e in range(num_episodes):
+        for episode_num in range(1, app_info['NUM_EPISODES']):
             state = env.fn_reset_env()
             state = np.reshape(state, (1, _state_size))
             score = 0
+            # print(f'EPISODE NUM: {episode_num}')
+            num_steps = 1
+            MAX_NUM_EPISODES = app_info['MAX_STEPS_PER_EPISODE'] + 1
+            reward = -9999
+            while (num_steps <= app_info['MAX_STEPS_PER_EPISODE']) or (reward >= app_info['REWARD_GOAL']):
 
-            num_steps = 0
-            for i in range(app_info['MAX_STEPS_PER_EPISODE']):
-                num_steps += 1
                 action = fnAct(state)
-                env.fn_render()
+                if episode_num ==  app_info['NUM_EPISODES']:
+                    env.fn_render()
+
                 next_state, reward, done, _ = env.fn_take_step(action)
                 score += reward
                 next_state = np.reshape(next_state, (1, _state_size))
                 fn_remember(state, action, reward, next_state, done)
                 state = next_state
                 fnReplay()
-                if done or (i == app_info['MAX_STEPS_PER_EPISODE'] - 1):
-                    print("episode: {}/{}, num of steps: {}, score: {}".format(e, num_episodes, num_steps, score))
-                    break
+                num_steps += 1
+
+            print("episode: {}/{}, num of steps: {}, score: {}".format(episode_num, app_info['NUM_EPISODES'], num_steps, score))
             loss.append(score)
 
         return loss
 
-    return fnTrain, fnSaveWeights, fnLoadWeights
+    cwd = __file__.rsplit('/', 1)[0]
+    model_dir = os.path.join(cwd, "models")
+    fnLoadWeights(model_dir)
+    fnTrain()
+
+    fnSaveWeights(model_dir)
+    env.fn_close()
+
+    # return fnTrain, fnSaveWeights, fnLoadWeights
 
 
 
