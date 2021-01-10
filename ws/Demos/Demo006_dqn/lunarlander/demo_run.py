9a90d939dc0bcd37ab5d08e908b5bfbee6f18919
basab-dattaray <basab.dattaray@gmail.com>
2021-01-09 23:45:37 -0800

    2.19.2 mac:

--- ws/Demos/Demo006_dqn/lunarlander/demo_run.py
+++ ws/Demos/Demo006_dqn/lunarlander/demo_run.py
@@ -1,5 +1,6 @@
 from ws.RLAgents.CAT2_value_based.fn_approx_yes__off_policy__bootstrap.dqn.agent_mgt import agent_mgt
 
 if __name__ == "__main__":
-    agent_mgt(__file__)
+    agent_mgt(__file__). \
+        fn_train()
 
--- ws/RLAgents/CAT2_value_based/fn_approx_yes__off_policy__bootstrap/dqn/agent_mgt.py
+++ ws/RLAgents/CAT2_value_based/fn_approx_yes__off_policy__bootstrap/dqn/agent_mgt.py
@@ -1,4 +1,5 @@
 import os
+from collections import namedtuple
 
 import numpy as np
 
@@ -11,48 +12,55 @@ def agent_mgt(caller_file):
     _state_size = env.fn_get_state_size()
 
     fn_reset_env, fn_remember, fnAct, fnReplay, fnSaveWeights, fnLoadWeights = impl_mgt(app_info, _state_size, _action_size)
-
-    def fnTrain():
-        # nonlocal fn_remember
-
-        # num_episodes = app_info['NUM_EPISODES']
-        loss = []
-        for episode_num in range(1, app_info['NUM_EPISODES']):
-            state = env.fn_reset_env()
-            state = np.reshape(state, (1, _state_size))
-            score = 0
-            # print(f'EPISODE NUM: {episode_num}')
-            num_steps = 1
-            MAX_NUM_EPISODES = app_info['MAX_STEPS_PER_EPISODE'] + 1
-            reward = -9999
-            while (num_steps <= app_info['MAX_STEPS_PER_EPISODE']) or (reward >= app_info['REWARD_GOAL']):
-
-                action = fnAct(state)
-                if episode_num ==  app_info['NUM_EPISODES']:
+    def fn_train():
+        def fn_run_training_episodes():
+            # nonlocal fn_remember
+    
+            num_episodes = app_info['NUM_EPISODES']
+            loss = []
+            for episode_num in range(1, num_episodes):
+                state = env.fn_reset_env()
+                state = np.reshape(state, (1, _state_size))
+                score = 0
+    
+                # num_steps = 0
+                for step_num in range(app_info['MAX_STEPS_PER_EPISODE']):
+                    # num_steps += 1
+                    action = fnAct(state)
+                    # if episode_num > app_info['NUM_EPISODES'] - 5:
                     env.fn_render()
+                    next_state, reward, done, _ = env.fn_take_step(action)
+                    score += reward
+                    next_state = np.reshape(next_state, (1, _state_size))
+                    fn_remember(state, action, reward, next_state, done)
+                    state = next_state
+                    fnReplay()
+    
+                    if done or (step_num == app_info['MAX_STEPS_PER_EPISODE'] - 1) or (score >= app_info['REWARD_GOAL']):
+                        print("episode: {}/{}, num of steps: {}, score: {}".format(episode_num, num_episodes, step_num, score))
+                        break
+                loss.append(score)
+    
+            return loss
+    
+        cwd = __file__.rsplit('/', 1)[0]
+        model_dir = os.path.join(cwd, "models")
+        fnLoadWeights(model_dir)
+        fn_run_training_episodes()
+    
+        fnSaveWeights(model_dir)
+        env.fn_close()
+        return agent_mgr
+        
+    agent_mgr = namedtuple('_', [
+        'fn_train',
+    ])
 
-                next_state, reward, done, _ = env.fn_take_step(action)
-                score += reward
-                next_state = np.reshape(next_state, (1, _state_size))
-                fn_remember(state, action, reward, next_state, done)
-                state = next_state
-                fnReplay()
-                num_steps += 1
+    agent_mgr.fn_train = fn_train
 
-            print("episode: {}/{}, num of steps: {}, score: {}".format(episode_num, app_info['NUM_EPISODES'], num_steps, score))
-            loss.append(score)
+    return agent_mgr
 
-        return loss
 
-    cwd = __file__.rsplit('/', 1)[0]
-    model_dir = os.path.join(cwd, "models")
-    fnLoadWeights(model_dir)
-    fnTrain()
-
-    fnSaveWeights(model_dir)
-    env.fn_close()
-
-    # return fnTrain, fnSaveWeights, fnLoadWeights
 
 
 
