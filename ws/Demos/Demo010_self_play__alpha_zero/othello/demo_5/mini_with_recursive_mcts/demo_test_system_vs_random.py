b92c0e5b27f0062017f20a72f144ca0b4a51595a
basab-dattaray <basab.dattaray@gmail.com>
2021-01-15 00:31:03 -0800

    2.27.4 mac:

--- ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/demo_test_system_vs_random.py
+++ ws/Demos/Demo010_self_play__alpha_zero/othello/demo_5/mini_with_recursive_mcts/demo_test_system_vs_random.py
@@ -1,8 +1,7 @@
-from ws.Demos.Demo010_self_play__alpha_zero.othello.demo_5.mini_with_recursive_mcts.ARGS import args
 from ws.RLAgents.CAT4_self_play.alpha_zero.agent_mgt import agent_mgt
 
 if __name__ == "__main__":
-    agent_mgt(args, __file__). \
+    agent_mgt(__file__). \
         fn_change_args({
             'NUM_MC_SIMULATIONS': 50,
             'NUM_TEST_GAMES': 12
