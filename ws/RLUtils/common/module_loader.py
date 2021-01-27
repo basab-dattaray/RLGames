0bc849e76724a41b2624c42a67f5d0db3e7219cf
basab-dattaray <basab.dattaray@gmail.com>
2021-01-26 20:38:00 -0800

    2.35.3.1 mac :

--- ws/RLUtils/common/module_loader.py
+++ ws/RLUtils/common/module_loader.py
@@ -9,16 +9,16 @@ def get_module(tag, package_tag):
         return None
 
 
-def load_function(function_name, module_tag, module_dot_path, package_tag=None):
+def load_function(function_name, module_name, module_dot_path, package_tag=None):
     MAX_ATTEMPT_COUNT = 7
-    tag = module_dot_path + '.' + module_tag
+    tag = module_dot_path + '.' + module_name
     obj = get_module(tag, package_tag)
 
     attempt_count = 0
     inner_subpackage_tag = module_dot_path
     while obj is None:
         inner_subpackage_tag = inner_subpackage_tag.rsplit('.', 1)[0]
-        obj = get_module(inner_subpackage_tag + '.' + module_tag, package_tag)
+        obj = get_module(inner_subpackage_tag + '.' + module_name, package_tag)
 
         attempt_count += 1
         if attempt_count > MAX_ATTEMPT_COUNT:
--- ws/RLUtils/setup/startup_mgt.py
+++ ws/RLUtils/setup/startup_mgt.py
@@ -31,7 +31,7 @@ def startup_mgt(caller_filepath):
     def fn_bootstrap(file_path):
         demo_folder_path, _ = fn_separate_folderpath_and_filename(file_path)
         demo_dot_path = fn_get_rel_dot_folder_path(demo_folder_path, '/ws/')
-        fn_get_args = load_function(function_name="fn_get_args", module_tag="ARGS", module_dot_path=demo_dot_path)
+        fn_get_args = load_function(function_name="fn_get_args", module_name="ARGS", module_dot_path=demo_dot_path)
         app_info = fn_get_args()
         app_info = _fn_init_arg_with_default_val(app_info, 'DEMO_FOLDER_PATH_', demo_folder_path)
         app_info = _fn_init_arg_with_default_val(app_info, 'DEMO_DOT_PATH_', demo_dot_path)
@@ -60,7 +60,7 @@ def startup_mgt(caller_filepath):
         if subpackage_name is None:
             return app_info, None
 
-        env_mgt = load_function(function_name="env_mgt", module_tag="env_mgt", module_dot_path=subpackage_name)
+        env_mgt = load_function(function_name="env_mgt", module_name="env_mgt", module_dot_path=subpackage_name)
 
         env = None
         if env_mgt is not None:
@@ -104,7 +104,7 @@ def startup_mgt(caller_filepath):
 
 
         agent_config_path = app_info.AGENTS_CONFIG_DOT_PATH + '.' + app_info.AGENT_CONFIG
-        fn_add_configs = load_function(function_name="fn_add_configs", module_tag="AGENT_CONFIG",
+        fn_add_configs = load_function(function_name="fn_add_configs", module_name="AGENT_CONFIG",
                                        module_dot_path=agent_config_path)
 
         fn_add_configs(app_info)
