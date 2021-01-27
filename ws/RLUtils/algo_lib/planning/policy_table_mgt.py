e9381e98b4efae407abc9f124ade146c4ab05f9f
basab-dattaray <basab.dattaray@gmail.com>
2021-01-27 06:48:43 -0800

    2.36.2.6 mac :

--- ws/RLUtils/algo_lib/planning/policy_table_mgt.py
+++ ws/RLUtils/algo_lib/planning/policy_table_mgt.py
@@ -1,6 +1,7 @@
 def policy_table_mgt(env):
     config = env.fn_get_config()
-    _policy_table = [[.25, .25, .25, .25] * config.DISPLAY['WIDTH'] for _ in range(config.DISPLAY['HEIGHT'])]
+    init_policy = [1/ env.fn_get_action_size()] * env.fn_get_action_size() # [.25, .25, .25, .25]
+    _policy_table = [init_policy * config.DISPLAY['WIDTH'] for _ in range(config.DISPLAY['HEIGHT'])]
 
     def fn_get_policy_state_value(state):
         return _policy_table[state[1]][state[0]]
