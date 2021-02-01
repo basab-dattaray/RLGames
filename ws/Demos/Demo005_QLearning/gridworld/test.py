b3869b4f2ed3dd60ba3cf96ec3c9c90bf81e10da
basab-dattaray <basab.dattaray@gmail.com>
2021-01-31 18:11:35 -0800

    2.43.2 Mac dev: introduced test for all gridwell demos

---
+++ ws/Demos/Demo005_QLearning/gridworld/test.py
@@ -0,0 +1,10 @@
+from ws.RLAgents.Category2_ValueBased.OffPolicy_Bootstrapping.qlearn.agent_mgt import agent_mgt
+
+if __name__ == "__main__":
+
+    agent_mgt(__file__). \
+        fn_set_test_mode(). \
+        fn_init()
+
+
+
--- ws/RLAgents/Category2_ValueBased/OnPolicy_Bootstrapping/sarsa/agent_mgt.py
+++ ws/RLAgents/Category2_ValueBased/OnPolicy_Bootstrapping/sarsa/agent_mgt.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict, namedtuple
 
+from ws.RLUtils.monitoring.tracing.tracer import tracer
 from .impl_mgt import impl_mgt
 
 from ws.RLUtils.setup.startup_mgt import startup_mgt
@@ -15,12 +16,21 @@ def agent_mgt(file_path):
         fn_bind_fn_display_actions(actions)
         return
 
+    @tracer(app_info, verboscity= 4)
+    def fn_set_test_mode():
+        app_info.ENV.display_mgr.fn_set_test_mode()
+        return agent_mgr
+
+
     agent_mgr = namedtuple('_',
                                 [
-                                    'fn_init'
+                                    'fn_init',
+                                    'fn_set_test_mode'
                                 ]
                            )
     agent_mgr.fn_init = fn_init
+    agent_mgr.fn_set_test_mode = fn_set_test_mode
 
     return agent_mgr
+
 
