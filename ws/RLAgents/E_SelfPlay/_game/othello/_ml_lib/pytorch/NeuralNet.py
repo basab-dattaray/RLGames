3afdfde653a7efdd1918fb9b6b90cf6fd3c1ad13
basab-dattaray <basab.dattaray@gmail.com>
2021-02-04 20:34:07 -0800

    2.54.2 Mac dev: streamlining neural_net_mgr

--- ws/RLAgents/E_SelfPlay/_game/othello/_ml_lib/pytorch/NeuralNet.py
+++ ws/RLAgents/E_SelfPlay/_game/othello/_ml_lib/pytorch/NeuralNet.py
@@ -9,8 +9,8 @@ import torch.nn.functional as F
 class NeuralNet(nn.Module):
     def __init__(self, game_mgr, nn_args):
         # game params
-        self.board_x, self.board_y = game_mgr.fn_get_board_size(), game_mgr.fn_get_board_size()
-        self.action_size = game_mgr.fn_get_action_size()
+        # self.board_x, self.board_y = game_mgr.fn_get_board_size(), game_mgr.fn_get_board_size()
+        # self.action_size = game_mgr.fn_get_action_size()
         self.app_info = nn_args
 
         super(NeuralNet, self).__init__()
